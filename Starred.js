96eb14f67c12907265d2f9b23b3d938897c85dcf
Anurag Anand <61843778+anuraganand2046@users.noreply.github.com>
2021-03-25 11:09:53 +0530

    Add files via upload

---
+++ Starred.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect} from 'react';
+import './Starred.css';
+import EmailListSettingLeft from './EmailListSettingLeft';
+import EmailListSettingRight from './EmailListSettingRight';
+import EmailRow from './EmailRow';
+import { db } from './firebase';
+function Starred(){
+    const [starred, setStarred]= useState([]);
+    useEffect(()=>{
+        db.collection('emails').orderBy('timestamp', 'desc').
+        onSnapshot(snapshot=> setStarred(snapshot.docs.map(doc=>({
+            id: doc.id,
+            data: doc.data(),
+        }))))
+    }, []);
+    return (
+        <div className= "starred">
+            <div className= "starred__settings">
+                {<EmailListSettingLeft />}
+                {<EmailListSettingRight />}
+            </div>
+            <div className= "starredList__list">
+                {starred.map(({id, data: {message, subject, timestamp, to}})=>(
+
+                    <EmailRow
+                        key= {id}
+                        id= {id}
+                        title= {to}
+                        description= {message}
+                        time= {new Date(timestamp?.seconds*1000).toUTCString()}
+                        subject= {subject}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+export default Starred;
